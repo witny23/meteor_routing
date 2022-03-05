88e1397f6836f95136449be23d96c86df41520ca
Michael Whitney <whitneym@winthrop.edu>
2022-03-05 00:08:24 -0500

    01b - Completed challenge: create Signup & Link components

--- url_shortener/client/main.jsx
+++ url_shortener/client/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from 'react';   
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom'; // importing named export
                                     // instead of ReactDOM
+import Signup from './../imports/ui/Signup.jsx';      // challenge 01
+import Link from './../imports/ui/Link.jsx';          // challenge 02
 
-
-// Challenge, breakout Signup into its own file
+// Challenge 01, breakout Signup into its own file
 // to do so:
 //    - inside of imports/ui
 //        - create the Signup.jsx
@@ -12,21 +13,23 @@ import { render } from 'react-dom'; // importing named export
 //    - import and use it in client/main.jsx
 
 
-class Signup extends React.Component {
-  render() {
-    return (
-      <p>Signup component placeholder</p>
-    );
-  }
-};
+// class Signup extends React.Component {
+//   render() {
+//     return (
+//       <p>Signup component placeholder</p>
+//     );
+//   }
+// };
 
 // Challenge 2, create a Link component
 // to do so:
 //  - create imports/ui/Link.jsx
-//  - import Link
-//  - render Link (just replace Signup in render)
+//  - import Link in main.jsx
+//  - render Link instead of Signup 
+//      - (just replace Signup with Link in render below)
 
 
 Meteor.startup(() => {
-  render(<Signup />, document.getElementById('react-target'));
+  // render(<Signup />, document.getElementById('react-target'));  // challenge 01
+  render(<Link />, document.getElementById('react-target'));    // challenge 02
 });
---
+++ url_shortener/imports/ui/Link.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default class Link extends React.Component {
+  render() {
+    return <p> Link component placeholder</p>;
+  }
+}
